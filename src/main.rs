e88d165e28d6750b77efd3d18258daa74fe5d948
Daniel Cooke <danielcooke1996@gmail.com>
2024-02-14 16:08:16 +0000

    Finish basic working program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use chrono::format::parse;
+use regex::Regex;
+use std::io::{self, Read};
+
 fn main() {
-    println!("Hello, world!");
+    let mut buffer = String::new();
+    io::stdin().read_to_string(&mut buffer).unwrap();
+
+    let rows = parse_input(&buffer);
+
+    println!("Date,Notes");
+    for row in rows {
+        println!("{},{}", row.date, row.notes)
+    }
+}
+
+struct TimesheetRow {
+    date: String,
+    notes: String,
+}
+
+fn parse_input(input: &str) -> Vec<TimesheetRow> {
+    let regex = Regex::new(r"Date:\s+([^\n]*)\n+(.*)").unwrap();
+
+    regex
+        .captures_iter(input)
+        .map(|x| {
+            let (_, [date, msg]) = x.extract::<2>();
+            TimesheetRow {
+                date: date.to_owned(),
+                notes: msg.to_owned(),
+            }
+        })
+        .collect()
 }
--- tests/main.rs
+++ tests/main.rs
@@ -2,16 +2,26 @@ use assert_cmd::Command;
 
 #[cfg(test)]
 mod tests {
+    use std::{
+        fs,
+        path::{self, Path, PathBuf},
+    };
+
     use assert_cmd::Command;
 
+    #[test]
     fn parses_git_log_from_stdin() {
+        let test_file = Path::new("tests/data").join("git-log-1");
+
+        let git_log = fs::read_to_string(test_file).unwrap();
+
+        let expected = "Date,Notes\n2021-01-01,Temporary commit\n2021-01-02,Another commit\n";
+
         Command::cargo_bin(env!("CARGO_PKG_NAME"))
             .unwrap()
-            .args(&["-"])
-            .write_stdin("commit 1\ncommit 2\n")
+            .write_stdin(git_log)
             .assert()
             .success()
-            .stdout("commit 1\ncommit 2\n"
+            .stdout(expected);
     }
-    
 }
